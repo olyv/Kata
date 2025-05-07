f9380d929f786e5c6655cfd0e213211a3dd8ece9
olyv <a.n.lyvin@gmail.com>
2025-05-07 18:13:41 +0200

    decode resistors colors

---
+++ Kata/test/DecodeResistorColorsTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DecodeResistorColorsTest {
+
+    @Test
+    public void TestOne() {
+        System.out.println("A resistor under 1000 ohms and with only three bands");
+        assertEquals("47 ohms, 20%", DecodeResistorColors.decodeResistorColors("yellow violet black"));
+    }
+    @Test
+    public void TestTwo() {
+        System.out.println("A resistor between 1000 and 999999 ohms, with a gold fourth band");
+        assertEquals("4.7k ohms, 5%", DecodeResistorColors.decodeResistorColors("yellow violet red gold"));
+    }
+    @Test
+    public void TestThree() {
+        System.out.println("A resistor of 1000000 ohms or above, with a silver fourth band");
+        assertEquals("1M ohms, 10%", DecodeResistorColors.decodeResistorColors("brown black green silver"));
+    }
+
+    @Test
+    public void TestFour() {
+
+        assertEquals("1k ohms, 10%", DecodeResistorColors.decodeResistorColors("brown black red silver"));
+    }
+}
